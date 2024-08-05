b40451d2bd9d396662610a1cb6f0bfeb998c9585
Naitik-554 <naitikp.official@gmail.com>
2024-08-05 12:19:11 +0530

    sorting with modular code done

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,114 +1,29 @@
-import { useEffect, useState } from "react";
+import Table from "./components/Table";
 import mockData from "./data";
-import "./App.css";
 
-const HeaderCell = ({ column, sorting, sortTable }) => {
-  const isDescSorting = sorting.column === column && sorting.ascending === false;
-  const isAscSorting = sorting.column === column && sorting.ascending === true;
-  const futureSortingOrder = isDescSorting ? true : false;
-  return (
-    <th
-      key={column}
-      className="users-table-cell"
-      onClick={() => sortTable({ column, ascending: futureSortingOrder })}
-    >
-      {column}
-      {isDescSorting && <span>▼</span>}
-      {isAscSorting && <span>▲</span>}
-    </th>
-  );
-};
-
-const Header = ({ columns, sorting, sortTable }) => {
-  return (
-    <thead>
-      <tr>
-        {columns.map((column) => (
-          <HeaderCell
-            column={column}
-            sorting={sorting}
-            key={column}
-            sortTable={sortTable}
-          />
-        ))}
-      </tr>
-    </thead>
-  );
-};
-
-const Content = ({ entries, columns }) => {
-  return (
-    <tbody>
-      {entries.map((entry) => (
-        <tr key={entry.id}>
-          {columns.map((column) => (
-            <td key={column} className="users-table-cell">
-              { entry[column] }
-            </td>
-          ))}
-        </tr>
-      ))}
-    </tbody>
-  );
-};
-
-const SearchBar = ({ searchTable }) => {
-  const [searchValue, setSearchValue] = useState("");
-  const submitForm = (e) => {
-    e.preventDefault();
-    searchTable(searchValue);
-  };
-  return (
-    <div className="search-bar">
-      <form onSubmit={submitForm}>
-        <input
-          type="text"
-          placeholder="Search here"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-        />
-      </form>
-    </div>
-  );
-};
+const columns = [
+  { label: "Id", accessor: "id", sortable: true,  sortbyOrder: "asc" },
+  { label: "Name", accessor: "name", sortable: false },
+  { label: "Age", accessor: "age", sortable: true },
+  { label: "Role", accessor: "role", sortable: false },
+  { label: "Hire date", accessor: "hireDate", sortable: true },
+  { label: "isActive", accessor: "isActive", sortable: true },
+  { label: "Salary", accessor: "salary", sortable: true },
+  { label: "Department", accessor: "department", sortable: true },
+  { label: "Projects", accessor: "projectsCompleted", sortable: true },
+  { label: "Last Login", accessor: "lastLogin", sortable: true },
+  { label: "Access", accessor: "accessLevel", sortable: true },
+];
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [sorting, setSorting] = useState({ column: "age", ascending: true });
-  const [searchValue, setSearchValue] = useState("");
-  const columns = ["id", "name", "age","role","hireDate","isActive","salary","department","projectsCompleted","lastLogin","accessLevel"];
-  function sortTable(key, ascending) {
-    setSorting({ key: key, ascending: ascending });
-  }
-  const searchTable = (newSearchValue) => {
-    setSearchValue(newSearchValue);
-  };
-
-  // useEffect(() => {
-  //   // console.log(mockData)
-  // }, [sorting, searchValue]);
-  
-  useEffect(()=> {
-    setUsers(mockData);
-  },[]);
-
-  // useEffect(() => {
-  //   const currentUsersCopy = [...users];
-  //   const sortedCurrentUsers = currentUsersCopy.sort((a, b) => {
-  //     return a[sorting.key].localeCompare(b[sorting.key]);
-  //   });
-  //   setUsers(
-  //     sorting.ascending ? sortedCurrentUsers : sortedCurrentUsers.reverse()
-  //   );
-  // }, [users, sorting]);
-
   return (
-    <div>
-      <SearchBar searchTable={searchTable} />
-      <table className="users-table">
-        <Header columns={columns} sorting={sorting} sortTable={sortTable} />
-        <Content entries={users} columns={columns} />
-      </table>
+    <div className="table_container">
+      <h1>Frontend Task</h1>
+      <Table
+        data={mockData}
+        columns={columns}
+      />
+      <br />
     </div>
   );
 };
